dfcde5e83dd5572531ca8445e8c9628807ad42fc
prateek-srivastav <prateek.srvastav@gmail.com>
2022-09-03 13:15:32 +0530

    fixed for build-2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
             Contact
           </p>
           <p className="text-gray-300 py-4">
-            // Submit the form below or shoot me an email -
+            {"//"} Submit the form below or shoot me an email -
             prateek.srvastav@gmail.com
           </p>
         </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,10 +5,8 @@ import CSS from "../assets/css.png";
 import JS from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
 import Node from "../assets/node.png";
-import Firebase from "../assets/firebase.png";
 import AWS from "../assets/aws.png";
 import Github from "../assets/github.png";
-import Tailwind from "../assets/tailwind.png";
 import Mongo from "../assets/mongo.png";
 
 const Skills = () => {
@@ -20,7 +18,9 @@ const Skills = () => {
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
             Skills
           </p>
-          <p className="py-4">// These are the technology I've worked with</p>
+          <p className="py-4">
+            {"//"} These are the technology I've worked with
+          </p>
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
